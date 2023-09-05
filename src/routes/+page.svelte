dbc9d0e653fd7927091c08a1f1a5738861b38709
Drew Fisher <drew.m.fisher@gmail.com>
2023-09-04 22:15:22 -0700

    Incorporate a palette and threadcount notation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,34 +3,267 @@
 
   let canvas: HTMLCanvasElement;
 
-  const blue = "042D51";
-  const brown = "4D3101";
-  const yellow = "E1B508";
-  const red = "B90004";
-  const green = "336B2B";
   const size = 480;
   const step = 2;
+
+  const paletteLabels = {
+    "LR": "light red",
+    "R": "red",
+    "DR": "dark red",
+    "O": "orange",
+    "DO": "dark orange",
+    "LY": "light yellow",
+    "Y": "yellow",
+    "DY": "dark yellow",
+    "LG": "light green",
+    "G": "green",
+    "DG": "dark green",
+    "LB": "light blue",
+    "B": "blue",
+    "DB": "dark blue",
+    "LP": "light purple",
+    "P": "purple",
+    "DP": "dark purple",
+    "W": "white",
+    "LN": "light grey",
+    "N": "grey",
+    "DN": "dark grey",
+    "K": "black",
+    "LT": "light brown",
+    "T": "brown",
+    "DT": "dark brown",
+  };
+
+  const palette = {
+    // Selected from among the many options at https://www.tartanregister.gov.uk/docs/Colour_shades.pdf
+    // See paletteLabels for names
+    "LR": [
+      "F4CCCC",
+      "E87878",
+      "F04DB0",
+    ],
+    "R": [
+      "A00048",
+      "FA4B00",
+      "FF0000",
+      "DC0000",
+      "C80000",
+      "C82828",
+      "C8002C",
+      "B03000",
+    ],
+    "DR": [
+      "A00000",
+      "960000",
+      "960000",
+      "880000",
+      "800028",
+      "781C38",
+      "4C0000",
+      "901C38",
+      "680028",
+    ],
+    "O": [
+      "EC8048",
+      "E86000",
+      "FF5000",
+      "DC943C",
+      "D87C00",
+    ],
+    "DO": [
+      "BE7832",
+    ],
+    "LY": [
+      "F9F5C8",
+      "F8E38C",
+    ],
+    "Y": [
+      "FFFF00",
+      "FFE600",
+      "FFD700",
+      "FCCC00",
+      "E0A126",
+      "E8C000",
+      "D8B000",
+    ],
+    "DY": [
+      "BC8C00",
+      "C89800",
+      "C88C00",
+    ],
+    "LG": [
+      "789484",
+      "C4BC68",
+      "9C9C00",
+      "ACD74A",
+      "86C67C",
+      "649848",
+    ],
+    "G": [
+      "008B00",
+      "408060",
+      "289C18",
+      "006400",
+      "007800",
+      "3F5642",
+      "767E52",
+      "5C6428",
+      "00643C",
+      "146400",
+      "006818",
+      "004C00",
+      "285800",
+      "005020",
+      "005448",
+    ],
+    "DG": [
+      "003C14",
+      "003820",
+      "004028",
+      "002814",
+    ],
+    "LB": [
+      "BCC3D2",
+      "98C8E8",
+      "82CFFD",
+      "00FCFC",
+    ],
+    "B": [
+      "048888",
+      "3C82AF",
+      "5C8CA8",
+      "2888C4",
+      "48A4C0",
+      "2474E8",
+      "0596FA",
+      "0000FF",
+      "3850C8",
+      "788CB4",
+      "5F749C",
+      "1870A4",
+      "1474B4",
+      "0000CD",
+      "2C4084",
+    ],
+    "DB": [
+      "055183",
+      "003C64",
+      "00008C",
+      "2C2C80",
+      "1C0070",
+      "000064",
+      "202060",
+      "000048",
+      "141E46",
+      "1C1C50",
+    ],
+    "LP": [
+      "A8ACE8",
+      "C49CD8",
+      "806D84",
+      "9C68A4",
+    ],
+    "P": [
+      "9058D8",
+      "AA00FF",
+      "B458AC",
+      "6C0070",
+      "5A008C",
+      "64008C",
+      "780078",
+    ],
+    "DP": [
+      "440044",
+      "1E0948",
+    ],
+    "W": [
+      "E5DDD1",
+      "E8CCB8",
+      "F0E0C8",
+      "FCFCFC",
+      "FFFFFF",
+      "F8F8F8",
+    ],
+    "LN": [
+      "E0E0E0",
+    ],
+    "N": [
+      "C8C8C8",
+      "C0C0C0",
+      "B0B0B0",
+      "A0A0A0",
+      "808080",
+      "888888",
+      "646464",
+    ],
+    "DN": [
+      "505050",
+      "555A64",
+      "1C1714",
+      "14283C",
+      "1C1C1C",
+    ],
+    "K": [
+      "101010",
+      "000000",
+    ],
+    "LT": [
+      "A08858",
+      "8C7038",
+      "A07C58",
+      "B07430",
+    ],
+    "T": [
+      "98481C",
+      "603800",
+      "604000",
+      "503C14",
+    ],
+    "DT": [
+      "4C3428",
+      "441800",
+      "230D00",
+    ],
+  };
 
   function makeChunk(length: number, color: string): string[] {
     const chunk = new Array(length).fill(color);
     return chunk;
   }
 
-  const colors = ([] as string[]).concat(
-    makeChunk(6, blue),
-    makeChunk(1, brown),
-    makeChunk(1, yellow),
-    makeChunk(1, brown),
-    makeChunk(1, blue),
-    makeChunk(5, brown),
-    makeChunk(5, green),
-    makeChunk(2, brown),
-    makeChunk(6, green),
-    makeChunk(5, brown),
-    makeChunk(6, blue),
-    makeChunk(1, red),
-    makeChunk(1, yellow),
-  );
+  const validColorCodes = Object.keys(palette);
+
+  // TODO: make this user-editable (select which color in the palette is used for e.g. "blue")
+  let activePaletteIndices = {};
+  for (const color of validColorCodes) {
+    activePaletteIndices[color] = 0;
+  }
+
+  const colorRe = new RegExp(`(\\d+)(${validColorCodes.join("|")})`);
+  function makeColor(c: string): [number, string] | undefined {
+    const m = c.match(colorRe);
+    if (m) {
+      return [parseInt(m[1], 10), m[2]];
+    }
+  }
+
+  function makeColors(colorsText: string): string[] {
+    const segments = colorsText.split(" ");
+    let retval = [];
+    for (const segment of segments) {
+      const color = makeColor(segment);
+      if (color !== undefined) {
+        const [threadCount, colorCode] = color;
+        const paletteIndex = activePaletteIndices[colorCode];
+        retval = retval.concat(makeChunk(threadCount, palette[colorCode][paletteIndex]));
+      }
+    }
+    return retval;
+  }
+
+  // TODO: make this user-editable
+  let colorString = "6B 1T 1Y 1T 1B 5T 5G 2T 6G 5T 6B 1R 1Y";
+  let colors = makeColors(colorString);
 
   const multiplier = size / colors.length;
   const lineWidth = size / 40 - 1;
